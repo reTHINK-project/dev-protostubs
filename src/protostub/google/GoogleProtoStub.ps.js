9c3518f0cb0fa5f9a6eee607364d70183858f511
RAFAEL SALGUEIRO FELGUEIRAS PINTO <P058076@PTPORTUGAL.CORPPT.COM>
2018-05-04 16:20:10 +0100

    abc

--- src/protostub/google/GoogleProtoStub.ps.js
+++ src/protostub/google/GoogleProtoStub.ps.js
@@ -58,6 +58,64 @@ class GoogleProtoStub {
 
     bus.addListener('*', (msg) => {
       console.log('[GoogleProtoStub] new Message  : ', msg);
+
+      if (msg.identity) {
+        _this._identity = msg.identity;
+      }
+
+      if (msg.body.identity) {
+        if (msg.body.identity.accessToken) {
+          _this._accessToken = msg.body.identity.accessToken;
+        }
+      }
+
+      if (_this._eb === null) {
+        _this._eb = new EventBus(config.url, { "vertxbus_ping_interval": config.vertxbus_ping_interval });
+        console.log('[GoogleProtoStub] Eventbus', _this._eb);
+
+        _this._eventBusUsage().then(function (result) {
+
+          const objectSchema = 'hyperty-catalogue://catalogue.localhost/.well-known/dataschema/Context';
+          const initialData = {
+            "id": "1276020076",
+            "values": [
+              {
+                "type": "user_walking_context",
+                "name": "walking distance in meters",
+                "unit": "meter",
+                "value": 1500,
+                "startTime": "2018-03-25T12:00:00Z",
+                "endTime": "2018-03-25T12:10:00Z"
+              },
+              {
+                "type": "user_biking_context",
+                "name": "biking distance in meters",
+                "unit": "meter",
+                "value": 5000,
+                "startTime": "2018-03-26T12:00:00Z",
+                "endTime": "2018-03-26T12:10:00Z"
+              }
+            ]
+          };
+
+          if (_this._identity.userProfile && _this._accessToken) {
+            _this._setUpReporter(_this._identity, objectSchema, initialData, ['context'], 'user_walking', _this._userURL).then(function (reporter) {
+              if (reporter) {
+                reporter.onSubscription((event) => event.accept());
+                console.log('[GoogleProtoStub] User activity DO created: ', reporter);
+                const startTime = reporter._created;
+
+                // invite observers
+                const observer = 'hyperty://sharing-cities-dsm/user-activity';
+                reporter.inviteObservers([observer]);
+
+                setInterval(function () { _this.querySessions(_this._accessToken, _this._identity.userProfile.userURL, startTime); }, config.sessions_query_interval);
+              }
+            });
+          }
+
+        });
+      }
     });
   }
 
@@ -95,15 +153,98 @@ class GoogleProtoStub {
       "data": JSON.stringify(bodyData)
     }
 
+
+
+
     $.ajax(settings).done(function (response) {
-      console.log(response);
+      console.log("[GoogleProtoStub] sessions: ", response);
+
+
+      // TODO - check if new session (last timestamp more recent than the one stored)
+      const lastSession = response.session[0];
+      const activityType = lastSession["activityType"];
+      const start = lastSession["startTimeMillis"];
+      const end = lastSession["endTimeMillis"];
+      const modified = lastSession["modifiedTimeMillis"];
+
+      let activity;
+
+      switch (activityType) {
+        case 7:
+          activity = "walking";
+          break;
+        case 8:
+          activity = "running";
+          break;
+        case 1:
+          activity = "biking";
+          break;
+        default:
+          break;
+      }
+      // get distance for session
+      _this.getDistanceForActivity(start, end, activity).then((result) => {
+        // console.log(result);
+        // send message to vertx
+        let msg = {
+          type: 'create',
+          to: userURL,
+          from: "me",
+          identity: { userProfile: { userURL: userURL } },
+          activity: activity,
+          distance: result
+        }
+        _this._eb.send(userURL, msg, function (reply_err, reply) {
+
+
+        });
+      });
+
+
     });
 
   }
 
-  getBikeDistance() {
+  getDistanceForActivity(start, end, activity) {
 
+    return new Promise((resolve, reject) => {
+
+      // total time
+      const durationMillis = end - start;
+
+      const bodyData = {
+        "aggregateBy": [{
+          "dataTypeName": "com.google.distance.delta"
+        }],
+        "bucketByTime": {
+          "durationMillis": durationMillis
+        },
+        "startTimeMillis": start,
+        "endTimeMillis": end
+      };
+
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + this._accessToken,
+          "Cache-Control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(bodyData)
+      }
+
+      $.ajax(settings).done(function (response) {
+        console.log("[GoogleProtoStub] distance for activity: ", response);
+
+        return resolve(response.bucket[0].dataset[0].point[0].value[0].fpVal);
+      });
+    });
   }
+
 
 
 
