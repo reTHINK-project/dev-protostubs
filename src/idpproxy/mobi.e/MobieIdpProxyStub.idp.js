bc5f28814e5286d5425bc457296fc333cab79990
Paulo Chainho <pchainho@gmail.com>
2018-09-17 11:30:36 +0100

    Mobi.e IdpProxy Initial version

---
+++ src/idpproxy/mobi.e/MobieIdpProxyStub.idp.js
@@ -0,0 +1,50 @@
+import { IdpProxy } from "../OAUTH"
+import { mobieAPIInfo, accessTokenAuthorisationEndpoint, accessTokenEndpoint, authorisationEndpoint, accessTokenInput, mapping, refreshAccessTokenEndpoint } from "./MobieInfo"
+import { convertUserProfile } from "./GoogleConverter"
+import AbstractIdpProxyProtoStub from "../AbstractIdpProxyStub"
+
+/**
+* Mobie Identity Provider Proxy Protocol Stub
+*/
+class MobieIdpProxyProtoStub extends AbstractIdpProxyProtoStub {
+
+  /**
+  * Constructor of the IdpProxy Stub
+  * The constructor add a listener in the messageBus received and start a web worker with the idpProxy received
+  *
+  * @param  {URL.RuntimeURL}                            runtimeProtoStubURL runtimeProtoSubURL
+  * @param  {Message.Message}                           busPostMessage     configuration
+  * @param  {ProtoStubDescriptor.ConfigurationDataList} configuration      configuration
+  */
+  constructor(runtimeProtoStubURL, bus, config) {
+    config.domain = 'mobie.com';
+    config.idpUrl = 'domain-idp://mobie.com';
+    config.idpProxy = IdpProxy;
+//    config.idpInfo = googleInfo;
+    config.apiInfo = mobieAPIInfo;
+    config.accessTokenAuthorisationEndpoint = accessTokenAuthorisationEndpoint;
+    config.accessTokenEndpoint = accessTokenEndpoint;
+    config.refreshAccessTokenEndpoint = refreshAccessTokenEndpoint;
+    config.accessTokenInput = accessTokenInput;
+    config.authorisationEndpoint = authorisationEndpoint;
+//    config.convertUserProfile = convertUserProfile;
+//    config.mapping = mapping;
+    super(runtimeProtoStubURL, bus, config);
+  }
+}
+
+// export default IdpProxyProtoStub;
+
+/**
+ * To activate this protocol stub, using the same method for all protostub.
+ * @param  {URL.RuntimeURL}                            runtimeProtoStubURL runtimeProtoSubURL
+ * @param  {Message.Message}                           busPostMessage     configuration
+ * @param  {ProtoStubDescriptor.ConfigurationDataList} configuration      configuration
+ * @return {Object} Object with name and instance of ProtoStub
+ */
+export default function activate(url, bus, config) {
+  return {
+    name: 'MobieIdpProxyProtoStub',
+    instance: new MobieIdpProxyProtoStub(url, bus, config)
+  };
+}
---
+++ src/idpproxy/mobi.e/MobieInfo.js
@@ -0,0 +1,122 @@
+/*
+Mainly used to authorise access to Mobi.e plataform through OAuth 2.0 protocol
+ */
+let redirectURI = location.protocol + '//' + location.hostname + (location.port !== '' ? ':' + location.port : '');
+/*
+export let googleInfo = {
+  "clientID": "808329566012-tqr8qoh111942gd2kg007t0s8f277roi.apps.googleusercontent.com",
+  "issuer": "https://accounts.google.com",
+  "tokenEndpoint": "https://www.googleapis.com/oauth2/v4/token?",
+  "jwksUri": "https://www.googleapis.com/oauth2/v3/certs?",
+  "authorisationEndpoint": "https://accounts.google.com/o/oauth2/auth?",
+  "userinfo": "https://www.googleapis.com/oauth2/v3/userinfo?access_token=",
+  "tokenInfo": "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=",
+  "accessType": "online",
+  "type": "token id_token",
+  "scope": "openid%20email%20profile",
+  "state": "state",
+  "domain": "google.com"
+};
+*/
+
+
+export let mobieAPIInfo = {
+  "clientID": "DSMSHARCITIES",
+//  "issuer": "https://accounts.google.com",
+  "tokenEndpoint": "https://www.googleapis.com/oauth2/v4/token?",
+  "revokeEndpoint": "https://accounts.google.com/o/oauth2/revoke?",
+//  "jwksUri": "https://www.googleapis.com/oauth2/v3/certs?",
+//  "authorisationEndpoint": "https://accounts.google.com/o/oauth2/auth?",
+//  "userinfo": "https://www.googleapis.com/oauth2/v3/userinfo?access_token=",
+//  "tokenInfo": "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=",
+//  "accessType": "offline",
+  "type": "code",
+// TODO - scope is read from message (support multiple Google APIs)
+//  "scope": "https://www.googleapis.com/auth/fitness.activity.read",
+//  "state": "state",
+  "domain": "mobie.pt",
+//  'grant_type': "authorization_code",
+  'secret': "Xx4rKucb5ZYTaXlcZX9HLfZW"
+};
+
+
+export function authorisationEndpoint(nonce) {
+
+  let url = mobieAPIInfo.authorisationEndpoint
+    + 'redirect_uri=' + redirectURI
+    + '&response_type=' + mobieAPIInfo.type
+    + '&client_id=' + mobieAPIInfo.clientID
+//    + '&scope=' + mobieAPIInfo.scope
+//    + '&access_type=' + mobieAPIInfo.accessType
+    + '&state=' + nonce
+    + '&BU=MOBI.E';
+  console.log('[MobieInfo.authorisationEndpoint] ', url);
+  return url;
+}
+
+export function accessTokenEndpoint(code, user) {
+
+  return mobieAPIInfo.tokenEndpoint
+    + 'client_id=' + mobieAPIInfo.clientID
+    + '&code=' + code
+    + '&grant_type=authorization_code'
+//    + '&access_type=' + 'offline'
+    + '&user_id=' + user
+    + '&client_secret=' + mobieAPIInfo.secret
+    + '&redirect_uri=' + redirectURI;
+}
+
+export function refreshAccessTokenEndpoint(refresh) {
+
+  return mobieAPIInfo.tokenEndpoint
+    + 'client_id=' + mobieAPIInfo.clientID
+    + '&refresh_token=' + refresh
+    + '&grant_type=refresh_token'
+    + '&client_secret=' + mobieAPIInfo.secret
+}
+
+export function revokeAccessTokenEndpoint(token) {
+
+  return mobieAPIInfo.revokeEndpoint
+    + '&token=' + token
+}
+
+export function mapping(resource) {
+  if (!resource) {
+    return "fitness.activity.read";
+  }
+  switch (resource) {
+    case "user_activity_context":
+      return "fitness.activity.read";
+      break;
+
+    default:
+      return "fitness.activity.read";
+      break;
+  }
+}
+
+
+export function accessTokenAuthorisationEndpoint(API) {
+  let url = googleAPIInfo.authorisationEndpoint
+    + 'redirect_uri=' + redirectURI
+    + '&response_type=' + googleAPIInfo.type
+    + '&client_id=' + googleAPIInfo.clientID
+    + '&scope=' + 'https://www.googleapis.com/auth/' + API
+    + '&access_type=' + googleAPIInfo.accessType
+//    + '&include_granted_scopes=true'
+//    + '&prompt=none'
+    + '&state=' + googleAPIInfo.state;
+  console.log('[GoogleInfo.accessTokenAuthorisationEndpoint] ', url);
+  return url;
+}
+
+
+export function accessTokenInput(info) {
+
+  return {info};
+}
+
+
+
+
