e23f806abf1b7c2a91823747600ee7f4dd553a0c
Paulo Chainho <pchainho@gmail.com>
2019-07-02 16:03:59 +0100

    copied assets from dev-hyperty to publish at github pages

---
+++ docs/demos/user-availability-observer/demo.js
@@ -0,0 +1,69 @@
+/*
+ * This is the Hello World App demo that uses the Hello World Reporter and Observer Hyperties
+ *
+ */
+
+//import {getTemplate, serialize} from './utils';
+
+let RUNTIME;
+const hypertyURI = (hyperty_domain, hyperty) => `hyperty-catalogue://catalogue.${hyperty_domain}/.well-known/hyperty/${hyperty}`;
+let runtime_domain = 'hybroker.rethink.ptinovacao.pt';
+let hyperty_domain = 'hybroker.rethink.ptinovacao.pt';
+let demoTemplate = 'https://rawgit.com/reTHINK-project/dev-hyperty/master/examples/user-availability/userAvailabilityObserver';
+let demoJs = 'https://rawgit.com/reTHINK-project/dev-hyperty/master/examples/user-availability/UserAvailabilityObserverDemo.js';
+
+let config = {
+  domain: hyperty_domain,
+  development: false,
+  runtimeURL: `hyperty-catalogue://catalogue.${runtime_domain}/.well-known/runtime/Runtime`
+};
+
+$(window).on( "load", function() {
+  console.log('ready');
+
+  loadRuntime();
+});
+
+/**
+* Function to load the Runtime
+*/
+
+function loadRuntime() {
+  var start = new Date().getTime();
+  //Rethink runtime is included in index.html
+  rethink.default.install(config).then((runtime) => {
+    RUNTIME = runtime
+    loadHyperty()
+  }).catch((reason) => {
+    console.error(reason);
+  });
+}
+
+/**
+* Function to load the Hyperty
+*/
+
+function loadHyperty()
+{
+  RUNTIME.requireHyperty(hypertyURI(hyperty_domain, 'UserAvailabilityObserver')).then((hyperty) => {
+    console.log('[UserAvailabilityObserverDemo.loadHyperty', hyperty);
+
+    getTemplate(demoTemplate, demoJs).then(function(template) {
+      let html = template();
+      $('.demo-content').html(html);
+
+      templateAreReady('user-availability');
+
+      if (typeof hypertyLoaded === 'function') {
+        hypertyLoaded(hyperty);
+      } else {
+        let msg = 'If you need pass the hyperty to your template, create a function called hypertyLoaded';
+        console.info(msg);
+        notification(msg, 'warn');
+      }
+
+      loading = false;
+
+    });
+  });
+}
